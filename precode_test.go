aef3bd9c27fa96d1bb51f81e9039fccdb0c4d637
Bogdan Sevostyanov <sbs2511@gmail.com>
2025-01-12 22:31:32 +0300

    1. Purple Haze

---
+++ precode_test.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"strconv"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+var cafeList = map[string][]string{
+	"moscow": []string{"Мир кофе", "Сладкоежка", "Кофе и завтраки", "Сытый студент"},
+}
+
+func mainHandle(w http.ResponseWriter, req *http.Request) {
+	countStr := req.URL.Query().Get("count")
+	if countStr == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("count missing"))
+		return
+	}
+
+	count, err := strconv.Atoi(countStr)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("wrong count value"))
+		return
+	}
+
+	city := req.URL.Query().Get("city")
+
+	cafe, ok := cafeList[city]
+	if !ok {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("wrong city value"))
+		return
+	}
+
+	if count > len(cafe) {
+		count = len(cafe)
+	}
+
+	answer := strings.Join(cafe[:count], ",")
+
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(answer))
+}
+
+func TestMainHandlerWhenRequestIsCorrectTheAnswerIsNotEmpty(t *testing.T) {
+	totalCount := 4
+	req := httptest.NewRequest("GET", "/cafe?count="+strconv.Itoa(totalCount)+"&city=moscow", nil)
+	// здесь нужно создать запрос к сервису
+
+	responseRecorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(mainHandle)
+	handler.ServeHTTP(responseRecorder, req)
+
+	require.Equal(t, http.StatusOK, responseRecorder.Code)
+	assert.Empty(t, responseRecorder.Body.String())
+}
+
+func TestMainHandlerWhenCityIsWrong(t *testing.T) {
+	wrongCity := "wrongcity"
+	correctError := "wrong city value"
+	req := httptest.NewRequest("GET", "/cafe?count=3&city="+wrongCity, nil)
+	// здесь нужно создать запрос к сервису
+
+	responseRecorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(mainHandle)
+	handler.ServeHTTP(responseRecorder, req)
+
+	require.Equal(t, http.StatusBadRequest, responseRecorder.Code)
+	assert.Equal(t, correctError, responseRecorder.Body.String())
+}
+
+func TestMainHandlerWhenCountMoreThanTheNumberOfCafes(t *testing.T) {
+	totalCount := 4
+	bigCount := 100
+	req := httptest.NewRequest("GET", "/cafe?count="+strconv.Itoa(bigCount)+"&city=moscow", nil)
+	// здесь нужно создать запрос к сервису
+
+	responseRecorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(mainHandle)
+	handler.ServeHTTP(responseRecorder, req)
+
+	require.Equal(t, http.StatusOK, responseRecorder.Code)
+	cafes := strings.Split(responseRecorder.Body.String(), ",")
+	assert.Len(t, cafes, totalCount)
+	assert.Equal(t, "Мир кофе,Сладкоежка,Кофе и завтраки,Сытый студент", responseRecorder.Body.String())
+}
